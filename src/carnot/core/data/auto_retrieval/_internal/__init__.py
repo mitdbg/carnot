084bdeca7e0da65db35252733f3fee83fcd2f8cb
zhuohangu <zhuohan@uchicago.edu>
2025-12-16 16:25:04 -0600

    feat: Concept Generation

--- src/carnot/core/data/auto_retrieval/_internal/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Internal implementation details for auto_retrieval module.
-
-This package contains non-public implementation details. Users should
-interact with the public API in the parent package instead.
-"""
-
