64790b3060f8456f4db47d73e821295008e9f0a3
Matthew Russo <mdrusso@mit.edu>
2026-04-08 15:37:45 -0400

    fix missing config setting

--- deploy/terraform/eks/main.tf
+++ deploy/terraform/eks/main.tf
@@ -41,7 +41,8 @@ resource "aws_eks_cluster" "carnot" {
   }
 
   access_config {
-    authentication_mode = "API_AND_CONFIG_MAP"
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions  = true
   }
 }
 
